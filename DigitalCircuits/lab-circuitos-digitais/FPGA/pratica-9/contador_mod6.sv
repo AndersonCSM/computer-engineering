2c9d92d06be9ee0b601102f72351329b43c398d3
Anderson Carlos <96142983+AndersonCSM@users.noreply.github.com>
2025-03-26 08:50:58 -0300

    +

--- DigitalCircuits/lab-circuitos-digitais/FPGA/pratica-9/contador_mod6.sv
+++ DigitalCircuits/lab-circuitos-digitais/FPGA/pratica-9/contador_mod6.sv
@@ -1,16 +1,15 @@
 module contador_mod6(
     input logic clk,
     input logic reset,
-    output logic [3:0] q
+    output logic [2:0] cont
 );
     
-    always_ff @(posedge clk) begin
-        if (reset) begin
-            q <= 0;
-        end else if (q == 5) begin
-            q <= 0;
-        end else begin
-            q <= q + 1;
-        end
+    always_ff @(posedge clk or negedge reset) begin
+        if (~reset)
+            cont <= 0;
+        else if (cont == 5)
+            cont <= 0;
+        else
+            cont <= cont + 1;
     end
 endmodule
